191d7658564aeb4ec1067e1b6988b744d8396ac0
Jose Nicolas Orellana Martinez <0003581@uca.edu.sv>
2019-09-03 23:34:24 -0600

    archivos 6

---
+++ actividad 6/martes 3.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <conio.h>
+
+using namespace std;
+
+int main (){
+    int numeros[]={1, 3, 4, 5, 17, 18, 31, 33};
+    int sup,inf,mitad,numero;
+    bool datofinal= false;
+
+    cout<<"digite el numero a buscar: "<<endl;
+    cin>>numero;
+    if (numero<0){
+        cout<<"error"<<endl;
+    }
+    inf =0;
+    sup=5;
+    while(inf<=sup){
+        mitad = (inf+sup)/2;
+        if (numeros[mitad]==numero){
+            datofinal=true;
+            break;
+        }
+        if (numeros[mitad] > numero){
+            sup= mitad;
+            mitad=(inf+sup)/2;
+        }
+        if (numeros[mitad]<numero){
+            inf = mitad;
+            mitad= (inf+sup)/2;
+        }
+
+        }
+    if (datofinal== true){
+        cout<<"el numero se encontro en la posicion: "<<mitad<<endl;
+    }
+    else{
+        cout<<"el numero no ha sido encontrado"<<endl;
+    }
+
+getch();
+return 0;
+}
