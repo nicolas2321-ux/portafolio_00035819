65ee188133b73c5902fabf0f29a3e8281fd4a3c8
Jose Nicolas Orellana Martinez <0003581@uca.edu.sv>
2019-09-10 22:01:05 -0600

    actividad #8 del portafolio

---
+++ actividad 8/actividad 8.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+using namespace std;
+
+struct Tpila{
+	float elements[100];
+	int top;
+};
+typedef struct Tpila Pila;
+void initialize(Pila *s){
+	s->top = -1;
+}
+
+bool vacio(Pila *s){
+	return s->top < 0;
+}
+
+void push(Pila *s, float e){
+	if(s->top < 99){
+		(s->top)++;
+		s->elements[s->top] = e;
+	}
+}
+
+void pop(Pila *s, int *e){
+	if(s->top >= 0){
+		*e = s->elements[s->top];
+		(s->top)--;
+	}
+}
+
+int main (){
+    Pila unaPila;
+	initialize(&unaPila);
+    int x=0;
+    int aux[100];
+    int cont=0;
+    for(int i=0; i<99; i++){
+        cout<<"digite el numero "<<i+1<<" de la pila"<<endl;
+        cout<<"si desea salir presione 0"<<endl;
+         cin>>aux[i];
+        if(aux[i]==0){
+            cout<<"has salido"<<endl;
+            i=100;}
+        else {
+            cont+=1;
+
+        }
+    }
+    for(int i=0; i<=cont; i++){
+        push(&unaPila,aux[i]);
+    }
+
+        cout<<"el primer elemento de la pila es: "<<aux[0]<<endl;
+
+
+}
